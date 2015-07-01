d8e74620682c97cb92d5566655893f6fd38236f6
Sudharsanan <sudhar.ti@gmail.com>
2015-07-01 18:11:26 -0500

    add routes, home controller, navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  devise_scope :user do
-    root to: 'devise/sessions#new'
-  end
+  root to: 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
