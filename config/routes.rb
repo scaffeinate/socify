ebc975af18163943cccc0cc384e9373eb34b77f3
Sudharsanan <sudhar.ti@gmail.com>
2015-07-01 00:15:03 -0500

    Routed to sign_in page, clean up asset pipeline

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  devise_scope :user do
+    root to: 'devise/sessions#new'
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
