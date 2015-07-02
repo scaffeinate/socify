9084ba6900b09b866b35144876002f7d4d63cf3a
Sudharsanan <sudhar.ti@gmail.com>
2015-07-01 19:16:37 -0500

    Devise views, forms & confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :confirmable,
+    :recoverable, :rememberable, :trackable, :validatable
 end
