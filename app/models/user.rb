421a87bb16b5c48d68db97a543e428812ec62e0b
Sudharsanan <sudhar.ti@gmail.com>
2015-07-03 17:35:51 -0500

    Acts as follow done, added to public activity

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :confirmable,
     :recoverable, :rememberable, :trackable, :validatable
   acts_as_voter
+  acts_as_follower
+  acts_as_followable
   has_many :posts
   has_many :comments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150703161656) do
+ActiveRecord::Schema.define(version: 20150703194107) do
 
   create_table "activities", force: true do |t|
     t.integer  "trackable_id"
@@ -44,6 +44,19 @@ ActiveRecord::Schema.define(version: 20150703161656) do
   add_index "comments", ["commentable_id"], name: "index_comments_on_commentable_id"
   add_index "comments", ["commentable_type"], name: "index_comments_on_commentable_type"
   add_index "comments", ["user_id"], name: "index_comments_on_user_id"
+
+  create_table "follows", force: true do |t|
+    t.integer  "followable_id",                   null: false
+    t.string   "followable_type",                 null: false
+    t.integer  "follower_id",                     null: false
+    t.string   "follower_type",                   null: false
+    t.boolean  "blocked",         default: false, null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "follows", ["followable_id", "followable_type"], name: "fk_followables"
+  add_index "follows", ["follower_id", "follower_type"], name: "fk_follows"
 
   create_table "posts", force: true do |t|
     t.text     "content",                     null: false
