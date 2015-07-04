4fdcc098b4d043cfd3576c3f614c9169cf7dd276
Sudharsanan <sudhar.ti@gmail.com>
2015-07-04 02:31:26 -0500

    Added attachment to post

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,4 +7,6 @@ class Post < ActiveRecord::Base
 
   include PublicActivity::Model
   tracked only: [:create], owner: Proc.new{ |controller, model| controller.current_user }
+
+  mount_uploader :attachment, AvatarUploader
 end
