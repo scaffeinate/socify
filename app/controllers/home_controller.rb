fddb8ec0795778df42ec3f60ac3de27989a779b1
Sudharsanan <sudhar.ti@gmail.com>
2015-07-06 20:56:43 -0500

    Intro text added, checking pagination works

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,11 +5,12 @@ class HomeController < ApplicationController
   def index
     @post = Post.new
     @friends = current_user.all_following
-    @activities = PublicActivity::Activity.where(owner_id: @friends.unshift(current_user)).paginate(page: params[:page], per_page: 10).order(created_at: :desc)
+    @activities = PublicActivity::Activity.where(owner_id: @friends.unshift(current_user))
+    .paginate(page: params[:page], per_page: 3).order(created_at: :desc)
   end
 
   def front
-    @activities = PublicActivity::Activity.paginate(page: params[:page], per_page: 10).order(created_at: :desc)
+    @activities = PublicActivity::Activity.paginate(page: params[:page], per_page: 4).order(created_at: :desc)
   end
 
   def find_friends
