cad4e6483a03fb4b6341e5263660055081dbd6ac
Sudharsanan <sudhar.ti@gmail.com>
2015-07-09 23:43:25 -0500

    More changes to ui

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,4 +2,9 @@ module UsersHelper
   def options_for_seasons
     [['Male', 'male'], ['Female', 'female']]
   end
+
+  def age(dob)
+    now = Time.now.utc.to_date
+    now.year - dob.year - (dob.to_date.change(:year => now.year) > now ? 1 : 0)
+  end
 end
