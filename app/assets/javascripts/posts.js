c9f14c9beb6b0f7974c589ba290d270a89d762b5
Sudharsanan <sudhar.ti@gmail.com>
2015-07-11 15:02:57 -0500

    render partials

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,9 +1,9 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 $(document).ready(function(){
-  if($("#endless-scroll").size() > 0) {
+  if($(".pagination").size() > 0) {
     $(".pagination").hide();
-    $("#loader").removeClass("hidden");
+    $("#endless-scroll").removeClass("hidden");
     $(window).bindWithDelay("scroll", function(){
       var url = $("a.next_page").attr("href");
       if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 50) {
