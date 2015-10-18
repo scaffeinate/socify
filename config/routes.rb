282c1182a0cf8a712d62c09142739b8c0b521802
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2015-10-18 15:31:21 -0500

    Social login form

--- config/routes.rb
+++ config/routes.rb
@@ -21,12 +21,12 @@ Rails.application.routes.draw do
     root 'home#front'
   end
 
-  match :follow, to: 'follows#create', as: :follow, via: :post
-  match :unfollow, to: 'follows#destroy', as: :unfollow, via: :post
-  match :like, to: 'likes#create', as: :like, via: :post
-  match :unlike, to: 'likes#destroy', as: :unlike, via: :post
-  match :find_friends, to: 'home#find_friends', as: :find_friends, via: :get
-  match :about, to: 'home#about', as: :about, via: :get
+  post :follow, to: 'follows#create', as: :follow
+  post :unfollow, to: 'follows#destroy', as: :unfollow
+  post :like, to: 'likes#create', as: :like
+  post :unlike, to: 'likes#destroy', as: :unlike
+  get :find_friends, to: 'home#find_friends', as: :find_friends
+  get :about, to: 'home#about', as: :about
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
