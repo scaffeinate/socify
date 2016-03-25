b04813399a05fbfee1639e43c43defb9ef5793e4
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2016-03-25 02:44:49 -0500

    Dropzone upload complete

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,19 +1,18 @@
 # encoding: utf-8
 
 class PhotoUploader < CarrierWave::Uploader::Base
-
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  storage :file if Rails.env.development?
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "uploads/photo_albums/#{model.photo_album.id}/"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
@@ -47,5 +46,4 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # def filename
   #   "something.jpg" if original_filename
   # end
-
 end
