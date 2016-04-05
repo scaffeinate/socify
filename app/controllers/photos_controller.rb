a7c3a5460094052d183672af644a8ebecaee063b
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2016-04-05 02:13:40 -0500

    Rethinking photo_albums and photos

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -5,10 +5,7 @@ class PhotosController < ApplicationController
 
     respond_to do |format|
       if @photo.save
-        if @photo_album.front_image_url.nil?
-          @photo_album.front_image_url = @photo.file.thumb.url
-          @photo_album.save
-        end
+        @photo_album.update!(front_image_url: @photo.file.thumb.url) if @photo_album.front_image_url.nil?
         format.json { render json: @photo }
       else
         format.json do
