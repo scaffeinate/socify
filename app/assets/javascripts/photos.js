1854bb729605af07ca8f3ebc19cf47cc5e544c13
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2016-05-18 02:15:07 -0500

    Close dropzone modal on upload

--- app/assets/javascripts/photos.js
+++ app/assets/javascripts/photos.js
@@ -16,3 +16,34 @@ $(document).ready(function() {
     $('#edit-photo-modal').modal('show');
   });
 });
+
+
+$(function() {
+  var photoDropzone = new Dropzone('#photo-dropzone');
+  Dropzone.options.photoDropzone = false;
+  photoDropzone.options.acceptedFiles = ".jpeg,.jpg,.png,.gif";
+
+  photoDropzone.on("complete", function(files) {
+    var _this = this;
+    if (_this.getUploadingFiles().length === 0 && _this.getQueuedFiles().length === 0) {
+      setTimeout(function(){
+        $('#upload-photos-modal').modal('hide');
+        var acceptedFiles = _this.getAcceptedFiles();
+        var rejectedFiles = _this.getRejectedFiles();
+
+        if(acceptedFiles.length != 0) {
+          alertify.success('Uploaded ' + acceptedFiles.length + ' files successfully.');
+        }
+        if(rejectedFiles.length != 0) {
+          alertify.error('Error uploading ' + rejectedFiles.length + ' files. Only image files are accepted.');
+        }
+
+        _this.removeAllFiles();
+
+        window.location.reload();
+
+      }, 1500);
+    }
+  });
+
+});
