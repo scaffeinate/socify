c6c0d6f2d2cf5a328462ecacf47b8f941214aa07
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2016-05-18 02:54:27 -0500

    Pagination for photos

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -4,6 +4,8 @@ class Photo < ActiveRecord::Base
 
   validates_presence_of :photo_album
 
+  self.per_page = 10
+
   scope :latest, -> (user = nil, limit = 10) do
     where(photo_album_id: user.photo_albums.map(&:id)).order('created_at desc').limit(limit)
   end
