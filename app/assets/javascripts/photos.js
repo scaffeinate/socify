ed708f2d9744db7c2f43a5cb90e22999a214c0a6
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2016-05-18 03:03:07 -0500

    Paginate photo_albums

--- app/assets/javascripts/photos.js
+++ app/assets/javascripts/photos.js
@@ -19,31 +19,34 @@ $(document).ready(function() {
 
 
 $(function() {
-  var photoDropzone = new Dropzone('#photo-dropzone');
-  Dropzone.options.photoDropzone = false;
-  photoDropzone.options.acceptedFiles = ".jpeg,.jpg,.png,.gif";
 
-  photoDropzone.on("complete", function(files) {
-    var _this = this;
-    if (_this.getUploadingFiles().length === 0 && _this.getQueuedFiles().length === 0) {
-      setTimeout(function(){
-        $('#upload-photos-modal').modal('hide');
-        var acceptedFiles = _this.getAcceptedFiles();
-        var rejectedFiles = _this.getRejectedFiles();
+  if($('#photo-dropzone').length) {
+    var photoDropzone = new Dropzone('#photo-dropzone');
+    Dropzone.options.photoDropzone = false;
+    photoDropzone.options.acceptedFiles = ".jpeg,.jpg,.png,.gif";
 
-        if(acceptedFiles.length != 0) {
-          alertify.success('Uploaded ' + acceptedFiles.length + ' files successfully.');
-        }
-        if(rejectedFiles.length != 0) {
-          alertify.error('Error uploading ' + rejectedFiles.length + ' files. Only image files are accepted.');
-        }
+    photoDropzone.on("complete", function(files) {
+      var _this = this;
+      if (_this.getUploadingFiles().length === 0 && _this.getQueuedFiles().length === 0) {
+        setTimeout(function(){
+          $('#upload-photos-modal').modal('hide');
+          var acceptedFiles = _this.getAcceptedFiles();
+          var rejectedFiles = _this.getRejectedFiles();
 
-        _this.removeAllFiles();
+          if(acceptedFiles.length != 0) {
+            alertify.success('Uploaded ' + acceptedFiles.length + ' files successfully.');
+          }
+          if(rejectedFiles.length != 0) {
+            alertify.error('Error uploading ' + rejectedFiles.length + ' files. Only image files are accepted.');
+          }
 
-        window.location.reload();
+          _this.removeAllFiles();
 
-      }, 1500);
-    }
-  });
+          window.location.reload();
+
+        }, 1500);
+      }
+    });
+  }
 
 });
