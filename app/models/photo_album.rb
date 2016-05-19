d6b55824dcefbcbefe20097032c1b12e5497685e
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2016-05-19 13:22:54 -0500

    Photo Album activity trackable, have to resize more

--- app/models/photo_album.rb
+++ app/models/photo_album.rb
@@ -1,13 +1,18 @@
 class PhotoAlbum < ActiveRecord::Base
   belongs_to :user
   has_many :photos, dependent: :destroy
+  acts_as_votable
+  acts_as_commentable
 
   validates_presence_of :title
   validates_presence_of :user
 
-  self.per_page = 10
+  include PublicActivity::Model
+  tracked only: [:create, :like], owner: proc { |_controller, model| model.user }
 
   default_scope -> { order('created_at DESC') }
+
+  self.per_page = 10
 
   extend FriendlyId
   friendly_id :title, use: [:slugged, :finders]
