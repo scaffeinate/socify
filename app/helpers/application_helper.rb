6b75488b3566169bc12ac5dd2ba9c5c29daddc07
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2016-08-02 18:52:29 -0500

    Fix everything when user deletes profile

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,8 @@ module ApplicationHelper
   def belongs_to_user?(resource)
     resource.user == current_user
   end
+
+  def activity_resources_exist?(activity)
+    activity && activity.trackable && activity.owner
+  end
 end
