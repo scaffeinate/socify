bf4302f5c68a9a5d3e8e2f9e0355932bcb4ec246
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2016-12-05 20:56:33 -0500

    Add helper for checking if user has about fields

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,15 +4,19 @@
 
 module UsersHelper
   def options_for_seasons
-    [['Male', 'male'], ['Female', 'female']]
+    [%w(Male male), %w(Female female)]
   end
 
   def age(dob)
     now = Time.now.utc.to_date
-    now.year - dob.year - (dob.to_date.change(:year => now.year) > now ? 1 : 0)
+    now.year - dob.year - (dob.to_date.change(year: now.year) > now ? 1 : 0)
   end
 
   def is_current_user?(user)
     user == current_user
+  end
+
+  def has_about_fields?(user)
+    !user.attributes.reject { |_k, v| v.present? }.empty?
   end
 end
