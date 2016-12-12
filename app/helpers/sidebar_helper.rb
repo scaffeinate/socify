25667b06d1e21152930e891333475d400fa2a8b6
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2016-12-11 20:20:48 -0500

    Rename helper methods

--- app/helpers/sidebar_helper.rb
+++ app/helpers/sidebar_helper.rb
@@ -3,11 +3,16 @@
 # under GNU GPL v2 or later. See the LICENSE.
 
 module SidebarHelper
-  def is_active(controller, action)
+  def is_active?(controller, action)
     'active' if params[:controller] == controller && params[:action] == action
   end
 
-  def is_user_page
+  def is_user_page?
     params[:controller] == 'users'
+  end
+
+  def has_about_fields?(user)
+    about_fields = %w(created_at dob location works_at hometown sex)
+    !user.attributes.select { |_k, v| about_fields.include?(_k) && v.present? }.empty?
   end
 end
