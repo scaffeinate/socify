22b3a36d72845089ccbb9b396b5cd1b4f7f659b4
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-03-11 23:22:58 -0500

    Show/hide the elements based on props for link and image preview components

--- app/assets/javascripts/components/image_preview.jsx
+++ app/assets/javascripts/components/image_preview.jsx
@@ -1,7 +1,7 @@
 var ImagePreview = React.createClass({
   render() {
     return (
-      <div className="img-preview">
+      <div className={this.props.src ? "img-preview" : "hidden"}>
         <img src={this.props.src}></img>
       </div>
     );
