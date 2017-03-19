bcab5fcddf987a1d495e3d012e2b6c368fd3a8f9
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-03-19 03:22:54 -0400

    CSS Preview fixes

--- app/assets/javascripts/components/link_preview.jsx
+++ app/assets/javascripts/components/link_preview.jsx
@@ -2,7 +2,7 @@ var LinkPreview = React.createClass({
   render() {
     return (
       <div className={this.props.html ? "link-preview" : "link-preview hidden"}>
-        <div className="autobox-preview form-group" dangerouslySetInnerHTML={{ __html: this.props.html }}></div>
+        <div className="onebox-preview form-group" dangerouslySetInnerHTML={{ __html: this.props.html }}></div>
         <input type="hidden" name={this.props.name} value={this.props.html} />
       </div>
     );
