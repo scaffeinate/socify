4358a4994998f57df91fee65d840da37d9a05f3d
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-03-19 02:43:57 -0400

    Img Preview fixes

--- app/assets/javascripts/components/image_preview.jsx
+++ app/assets/javascripts/components/image_preview.jsx
@@ -1,7 +1,8 @@
-var ImagePreview = React.createClass({
+const ImagePreview = React.createClass({
   render() {
+    var className = this.props.className || 'img-preview';
     return (
-      <div className={this.props.src ? "img-preview" : "hidden"}>
+      <div className={this.props.src ? className : 'hidden'}>
         <img src={this.props.src}></img>
       </div>
     );
