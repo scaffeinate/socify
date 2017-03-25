fe094b6c07a18870ce9349d75b1691776856f6e6
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-03-25 16:30:21 -0400

    Add EventsForm react component

--- app/assets/javascripts/components/events/form.jsx
+++ app/assets/javascripts/components/events/form.jsx
@@ -2,11 +2,14 @@ DateTimePickerModal = require('../datetimepickermodal.jsx');
 var EventsForm = React.createClass({
   getInitialState() {
     return {
-
+      address: ''
     }
   },
   handleClick() {
     $('#datetimepickermodal').modal('show');
+  },
+  handlePlaceChange(address) {
+    this.setState({address: address});
   },
   render() {
     return(
@@ -19,6 +22,10 @@ var EventsForm = React.createClass({
           <div className="form-group">
             <label>When is it?</label>
             <input type='text' placeholder='When is it?' name='when' value={this.state.date} className='form-control' onClick={this.handleClick} />
+          </div>
+          <div className="form-group">
+            <label>Location</label>
+            <PlacesAutocomplete value={this.state.address} onChange={this.handlePlaceChange} classNames={{input:'form-control'}} />
           </div>
         </form>
       </div>
