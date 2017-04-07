651f7c06532cebc4fbaa8500460871bbfd8ccbd0
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-04-07 00:40:18 -0400

    Finish up the UI part of StatusActions

--- app/assets/javascripts/components/events/status_actions.jsx
+++ app/assets/javascripts/components/events/status_actions.jsx
@@ -1,9 +1,39 @@
 var EventStatusActions = React.createClass({
+  getDefaultProps() {
+    return {
+      statusItems: [
+        { label: 'Not Going', value: 0, icon: 'icon-cancel-1' },
+        { label: 'Interested', value: 1, icon: 'icon-bookmark' },
+        { label: 'Going', value: 2, icon: 'icon-ok' }
+      ]
+    }
+  },
+  getInitialState() {
+    return {
+      baseClasses: 'btn btn-sm btn-default',
+      currentStatusItem: this.props.status[0]
+    }
+  },
+  handleClick(value, e) {
+    e.preventDefault();
+    this.setState({currentStatusItem: value});
+  },
+  constructClassName(value) {
+    return 'btn btn-sm btn-default ' +
+          ((this.state.currentStatusItem == value) ? 'active' : '');
+  },
   render() {
+    var _this = this;
+    var statusItems = this.props.statusItems.map(function(statusItem) {
+      return (
+        <a key={statusItem.value} className={_this.constructClassName(statusItem.value)} href='#' onClick={(evt) => _this.handleClick(statusItem.value, evt)}>
+          <i className={statusItem.icon}></i> {statusItem.label}
+        </a>
+      )
+    });
     return (
       <div className="status-actions">
-        <a href='#' className='btn btn-sm btn-default'><i className='icon-bookmark-empty'></i> Interested</a>
-        <a href='#' className='btn btn-sm btn-default'><i className='icon-ok'></i> Going</a>
+        {statusItems}
       </div>
     )
   }
