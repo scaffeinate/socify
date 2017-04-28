d5885ab25171d43490cba996fcfd9808c9f4a935
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-04-28 00:20:29 -0400

    Add events.js to fetch events into calendar

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,2 +1,14 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(document).ready(function () {
+  var friends_calendar = $('#calendar').fullCalendar({
+    defaultView: 'month',
+    events: {
+      url: '/events/calendar',
+      type: 'GET',
+      error: function () {
+        alert('There was an error while fetching events.');
+      }
+    }
+  });
+});
