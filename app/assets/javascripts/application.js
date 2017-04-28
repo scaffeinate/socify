5836738a9d896702eeefb517e5e5165b73ce9abd
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-04-28 00:20:14 -0400

    Add fullcalendar library

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,16 +15,12 @@
 //= require jquery_ujs
 //= require bootstrap
 //= require bindWithDelay
+//= require moment.min
+//= require fullcalendar.min
 //= require jquery.datetimepicker
 //= require_tree .
 
-$(document).ready(function(){
-  $("#event_when").datetimepicker({
-    format:'Y/m/d H:i'
-  });
-  $("#user_dob").datetimepicker({
-    timepicker:false,
-    format:'Y/m/d',
-    maxDate:'0'
-  });
+$(document).ready(function () {
+  $("#event_when").datetimepicker({format: 'Y/m/d H:i'});
+  $("#user_dob").datetimepicker({timepicker: false, format: 'Y/m/d', maxDate: '0'});
 });
