45c8cfa8664ee2fee53a815c96c0242339d63e29
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-04-27 23:15:38 -0400

    Fix posts edit

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,10 +1,10 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-$(document).ready(function() {
+$(document).ready(function () {
   if ($(".pagination").size() > 0) {
     $(".pagination").hide();
     $("#endless-scroll").removeClass("hidden");
-    $(window).bindWithDelay("scroll", function() {
+    $(window).bindWithDelay("scroll", function () {
       var url = $("a.next_page").attr("href");
       if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 50) {
         $.getScript(url);
@@ -12,15 +12,11 @@ $(document).ready(function() {
     }, 150);
   }
 
-  $('.input-mentionable').atwho({
-    at: '@',
-    data: $('#mentionable-data').data('content'),
-    insertTpl: '<a href="/users/${id}">${name}</a>',
-    displayTpl: '<li data-id="${id}"><span>${name}</span></li>',
-    limit: 15
-  });
+  $('#post-content').html($('#post_content').val());
 
-  $('.post_form').submit(function() {
+  $('.input-mentionable').atwho({at: '@', data: $('#mentionable-data').data('content'), insertTpl: '<a href="/users/${id}">${name}</a>', displayTpl: '<li data-id="${id}"><span>${name}</span></li>', limit: 15});
+
+  $('.post_form').submit(function () {
     $('#post_content').val($('#post-content').html());
     $('#post-content').html('');
   });
