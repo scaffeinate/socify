37147f7ef3d0deefb4a9f526beb8cbc4c5413f53
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-04-30 13:44:41 -0400

    Finish up all components cleanup

--- app/assets/javascripts/components/comments/CommentsIndex.jsx
+++ app/assets/javascripts/components/comments/CommentsIndex.jsx
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import fetch from 'isomorphic-fetch';
 import CommentsList from './CommentsList';
 import CommentsForm from './CommentsForm';
+
+/* global FormData */
 
 const propTypes = {
   userSignedIn: PropTypes.boolean, // TODO: Remove this and use global state
@@ -22,31 +25,108 @@ class CommentsIndex extends Component {
     super(props);
 
     this.state = {
-      data: []
+      comments: []
     };
 
-    this.getComments = this.getComments.bind(this);
     this.onFormSubmit = this.onFormSubmit.bind(this);
     this.onCommentDelete = this.onCommentDelete.bind(this);
-  }
-
-  getComments() {
-
-  }
-
-  onFormSubmit(event, params) {
-  }
-
-  onCommentDelete(event, id) {
-
+    this.getComments = this.getComments.bind(this);
   }
 
   componentDidMount() {
+    this.getComments();
+  }
 
+  onFormSubmit(event, params) {
+    event.preventDefault();
+    if (params) {
+      const self = this;
+      const authenticityToken = this.props.authenticityToken;
+      const formData = new FormData();
+      formData.append('commentable_type', this.props.commentableType);
+      formData.append('commentable_id', this.props.commentableId);
+      fetch('/comments', {
+        method: 'POST',
+        body: formData,
+        headers: {
+          'X-Requested-With': 'XMLHttpRequest',
+          'X-CSRF-Token': authenticityToken
+        },
+        credentials: 'same-origin'
+      })
+      .then(response => response.json())
+      .then((data) => {
+        const comments = self.state.comments.append(data);
+        self.setState({ comments });
+      })
+      .catch(e => console.error(e));
+    }
+  }
+
+  onCommentDelete(event, commentId) {
+    event.preventDefault();
+    if (commentId) {
+      const self = this;
+      const authenticityToken = this.props.authenticityToken;
+      fetch(`/comments/${commentId}`, {
+        method: 'DELETE',
+        headers: {
+          'X-Requested-With': 'XMLHttpRequest',
+          'X-CSRF-Token': authenticityToken
+        },
+        credentials: 'same-origin'
+      })
+      .then(response => response.json())
+      .then((data) => {
+        const comments = self.state.data.filter(({ comment }) => (comment.id !== data.id));
+        self.setState({ comments });
+      })
+      .catch(e => console.error(e));
+    }
+  }
+
+
+  getComments() {
+    const self = this;
+    const authenticityToken = this.props.authenticityToken;
+    const formData = new FormData();
+    formData.append('commentable_type', this.props.commentableType);
+    formData.append('commentable_id', this.props.commentableId);
+
+    fetch('/comments', {
+      method: 'GET',
+      body: formData,
+      headers: {
+        'X-Requested-With': 'XMLHttpRequest',
+        'X-CSRF-Token': authenticityToken
+      },
+      credentials: 'same-origin'
+    })
+    .then(response => response.json())
+    .then(data => self.setState({ comments: data }))
+    .catch(e => console.error(e));
   }
 
   render() {
-    return (<div>CommentsIndex</div>);
+    // TODO: authenticate user and hide/show
+    const clsName = (this.props.userSignedIn || this.state.comments.length !== 0) ? 'comments-block' : '';
+    return (
+      <div className={clsName}>
+        <div className="row">
+          <CommentsForm
+            authenticityToken={this.props.authenticityToken}
+            userLinkTo={this.props.userLinkTo}
+            userAvatar={this.props.userAvatar}
+            onSubmit={this.onFormSubmit}
+          />
+        </div>
+        <div className="row">
+          <div className="comments">
+            <CommentsList data={this.state.comments} onDelete={this.onCommentDelete} />
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
