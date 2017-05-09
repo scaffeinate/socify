d69c218e2e9f78865b41d3e97b2186ec1337b655
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-05-09 02:28:51 -0400

    Cleaned up a little

--- app/assets/javascripts/components.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Application from './containers/Application';
-
-global.React = React;
-global.ReactDOM = ReactDOM;
-global.Application = Application;
