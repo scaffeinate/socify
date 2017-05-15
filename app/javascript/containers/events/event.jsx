5eece00463ebb6ea24aa81c9699ee4f500d2d77d
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-05-14 21:01:27 -0500

    Change to ES6 for Events index and Event component

--- app/javascript/containers/events/event.jsx
+++ app/javascript/containers/events/event.jsx
@@ -1,41 +1,45 @@
-var Event = React.createClass({
-  render() {
-    var event = this.props.event;
-    var user = event.user;
-    var user_path = '/users/' + user.id;
-    <div className="event">
+import React from 'react';
+import PropTypes from 'prop-types';
+import EventActions from './EventActions';
+import UserInfo from '../../components/UserInfo';
+import ImagePreview from '../../components/ImagePreview';
+import CreatedAt from '../../components/CreatedAt';
+
+const propTypes = {
+  event: PropTypes.object.isRequired,
+  onEventDelete: PropTypes.func.isRequired
+};
+
+const Event = ({ event, onEventDelete }) => (
+  <div className="event">
+    <div className="row">
+      <UserInfo user={event.user} />
+      <div className="actions">
+        <EventActions onEventDelete={onEventDelete} />
+      </div>
       <div className="row">
-        <div className="event-user-info">
-          <Avatar linkTo={user_path} avatar={user.avatar} />
-          <h4 className="name">
-            <a href={user_path}>{user.name}</a>
-          </h4>
+        <div className="event-created-at">
+          <CreatedAt isoTime={event.created_at} format={0} />
         </div>
-        <div className="actions">
-          <Actions isEditable={this.props.isEditable} event={event} />
-        </div>
-        <div className="row">
-          <div className="event-created-at">
-            <CreatedAt isoTime={event.created_at} format={0} />
-          </div>
-        </div>
-        <div className="row">
-          <div className="event-content">
-            <ImagePreview src={event.attachment.file_name.url} clsName={'img-fit'} />
-            <div className="event-details">
-              <span className="text">{event.name}</span>
-              <div className="calendar">
-                <i className="icon-calendar-empty"></i> <CreatedAt isoTime={event.when} format={1} />
-              </div>
-              <div className="location">
-                <i className="icon-location-1"></i>{(event.location) || 'Unknown'}
-              </div>
+      </div>
+      <div className="row">
+        <div className="event-content">
+          <ImagePreview src={event.attachment.file_name.url} clsName={'img-fit'} />
+          <div className="event-details">
+            <span className="text">{event.name}</span>
+            <div className="calendar">
+              <i className="icon-calendar-empty" /><CreatedAt isoTime={event.when} format={1} />
+            </div>
+            <div className="location">
+              <i className="icon-location-1" />{(event.location) || 'Unknown'}
             </div>
           </div>
         </div>
       </div>
     </div>
-  }
-});
+  </div>
+);
 
-module.exports = Event;
+Event.propTypes = propTypes;
+
+export default Event;
--- app/javascript/containers/events/index.jsx
+++ app/javascript/containers/events/index.jsx
@@ -1,34 +1,35 @@
-var Event = require('./event.jsx');
-var EventTabs = require('./_tabs.jsx');
-var EventStatusActions = require('./_status_actions.jsx');
-var Events = React.createClass({
-  render() {
-    var events = this.props.data.map(function(event, index) {
-      return '';
-    });
-    return (
-      <div id="events-stream" className="stream">
-        <div className="new-resource">
-          <div className="row">
-            <div className="col-xs-8">
-              <EventTabs authenticityToken={this.props.authenticityToken} />
-            </div>
-            <div className="col-xs-4">
-              <a href='/events/new' className='btn btn-sm btn-primary pull-right'>
-                <i className="icon-plus"></i> {'New Event'}
-              </a>
-            </div>
-          </div>
+import React from 'react';
+import PropTypes from 'prop-types';
+import EventTabs from './EventTabs';
+
+const propTypes = {
+  authenticityToken: PropTypes.string.isRequired, // TODO: Remove this and use global CSRF-Token
+  events: PropTypes.object.isRequired
+};
+
+const Events = ({ events, authenticityToken }) => (
+  <div id="events-stream" className="stream">
+    <div className="new-resource">
+      <div className="row">
+        <div className="col-xs-8">
+          <EventTabs authenticityToken={authenticityToken} />
         </div>
-        <div id="events" className="resources-block newsfeed">
-          <h4 className="underlined">EXPLORE EVENTS</h4>
-          {events}
-        </div>
-        <div className="pagination-block">
+        <div className="col-xs-4">
+          <a href="/events/new" className="btn btn-sm btn-primary pull-right">
+            <i className="icon-plus" /> {'New Event'}
+          </a>
         </div>
       </div>
-    );
-  }
-});
+    </div>
+    <div id="events" className="resources-block newsfeed">
+      <h4 className="underlined">EXPLORE EVENTS</h4>
+      {events} {/* TODO: Render EventsList */}
+    </div>
+    {/* TODO: Use pagination block component */}
+    <div className="pagination-block" />
+  </div>
+);
 
-module.exports = Events;
+Events.propTypes = propTypes;
+
+export default Events;
