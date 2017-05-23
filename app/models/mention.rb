e2fff711e5281acef2269adbff5032a0156fb374
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-05-23 18:17:55 -0400

    Update mention.rb

--- app/models/mention.rb
+++ app/models/mention.rb
@@ -28,7 +28,7 @@ private
 
   def add_mention_links_to_content
     mentioned_friends.each do |friend_name|
-      new_content = html_type.gsub!(friend_name, "<a href='/users/#{User.where(name: friend_name).first.id}'>#{friend_name}</a>")
+      new_content = html_type.gsub!(friend_name, "<a href='/users/#{User.where(name: friend_name).first.slug}'>#{friend_name}</a>")
       self.content = new_content if is_a?(Post)
       self.comment = new_content if is_a?(Comment)
     end
