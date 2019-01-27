e32bd96c3265156dd1979b99d782efdb25f6f094
Phong Truong <phongtruongit@gmail.com>
2019-01-27 09:02:01 +0700

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   has_merit
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :confirmable,
-    :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+  
   acts_as_voter
   acts_as_follower
   acts_as_followable
